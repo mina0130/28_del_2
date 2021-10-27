a13c6c5c2601afc919f6b35a6e2e8a70a175b8d5
mohammedbhatti <s215712@dtu.dk>
2021-10-27 14:30:12 +0200

    pom fil gui ny

---
+++ src/main/java/GUI.java
@@ -0,0 +1,8 @@
+import gui_fields.GUI_Street;
+
+
+public class GUI {
+    public static void main(String[] args) {
+        //GUI_Street = new GUI();
+    }
+}
--- src/main/java/main/java/player.java
+++ src/main/java/main/java/player.java
@@ -1,43 +1,19 @@
 package main.java;
 
-
-import gui_fields.GUI_Player;
-import gui_main.GUI;
-
-//import gui_fields.GUI_Field;
+import java.awt.*;
 
 public class player {
-    private static GUI gui;
-    private static GUI_Player GUI_player;
-
-
     static String[] player1name= new String[2];
     static int[] playerbalance = new int[2];
     static int count=0;
 
 
 
+
     public static void addPlayer(String name, int balance){
-
-
-
-        GUI_Player GUI_player = new GUI_Player(player1name[count], playerbalance[count]);
-
-        //gui.addPlayer(GUI_player);
-        player.setGUI_player(GUI_player);
-        player.getGUI_player().getCar();
-
-
 
         player1name[count]=name;
         playerbalance[count]=balance;
-    }
-
-    public static void setGUI_player(GUI_Player gUI_player) {
-        GUI_player = gUI_player;
-    }
-    public static GUI_Player getGUI_player() {
-        return GUI_player;
     }
 
 }
